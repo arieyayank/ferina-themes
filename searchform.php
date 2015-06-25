597049037e165091c0877ff3e124752493e18924
Eko Prasetyo <info@pradesga.com>
2015-06-26 06:11:59 +0700

    Create searchform.php

---
+++ searchform.php
@@ -0,0 +1,5 @@
+<form method="get" action="<?php echo home_url('/'); ?>" class="ferina-search-form">
+	<label for="s"><?php echo __('Search the site', 'Ferina');?> : </label>
+	<input type="text" id="s" name="s" value="<?php the_search_query(); ?>">
+	<input type="submit" value="<?php echo __('Search', 'Ferina');?>">
+</form>
